3dd358b449b314f1b157e354c325ff2290c1ea62
Barrero Diego <10062583@gemalto.com>
2020-07-16 20:23:35 -0500

    Add Domain

---
+++ MAS.Domain/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MAS.Domain
+{
+    /// <summary>
+    /// Defines the <see cref="Employee" />.
+    /// </summary>
+    public class Employee
+    {
+        /// <summary>
+        /// Gets or sets the Id.
+        /// </summary>
+        public Int64 Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ContractTypeName.
+        /// </summary>
+        public string ContractTypeName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the RoleId.
+        /// </summary>
+        public int RoleId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the RoleName.
+        /// </summary>
+        public string RoleName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the RoleDescription.
+        /// </summary>
+        public string RoleDescription { get; set; }
+
+        /// <summary>
+        /// Gets or sets the HourlySalary.
+        /// </summary>
+        public long HourlySalary { get; set; }
+
+        /// <summary>
+        /// Gets or sets the MonthlySalary.
+        /// </summary>
+        public long MonthlySalary { get; set; }
+    }
+}
