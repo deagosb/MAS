bada4647ea4996fabd66963f29fa51d18ec7e7d7
LEGION Y520 <deagosb@gmail.com>
2020-07-16 22:09:41 -0500

    Add IRepository

---
+++ MAS.Data/IEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using MAS.Domain;
+using System.Collections.Generic;
+
+namespace MAS.Data
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int EmployeeID);
+        void Insert(Employee employee);
+        void Update(Employee employee);
+        void Delete(int EmployeeID);
+        void Save();
+    }
+}
