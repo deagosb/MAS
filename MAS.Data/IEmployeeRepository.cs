a4231a3d34512fbbe9a1d7e39a9d1e080100d5ae
Barrero Diego <deagosb@gmail.com>
2020-07-17 15:26:51 -0500

    Remove unnecessary operations from IRepository

--- MAS.Data/IEmployeeRepository.cs
+++ MAS.Data/IEmployeeRepository.cs
@@ -8,8 +8,8 @@ namespace MAS.Data
         IEnumerable<Employee> GetAll();
         Employee GetById(int EmployeeID);
         void Insert(Employee employee);
-        void Update(Employee employee);
-        void Delete(int EmployeeID);
-        void Save();
+        //void Update(Employee employee);
+        //void Delete(int EmployeeID);
+        //void Save();
     }
 }
