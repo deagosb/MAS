6b248c20481c83dbe5e701a7b00078d3d423f2b1
Barrero Diego <deagosb@gmail.com>
2020-07-17 15:59:07 -0500

    Implements methods in EmployeeAPIRepository

--- MAS.Data/EmployeeAPIRepository .cs
+++ MAS.Data/EmployeeAPIRepository .cs
@@ -1,5 +1,9 @@
 ﻿using MAS.Domain;
+using RestClient.Net;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MAS.Data
 {
@@ -7,12 +11,22 @@ namespace MAS.Data
     {
         public IEnumerable<Employee> GetAll()
         {
-            return new List<Employee>();
+            return GetEmployeeList();
         }
 
         public Employee GetById(int EmployeeID)
         {
-            return new Employee();
+            IEnumerable<Employee> employees = GetEmployeeList();
+
+            return employees.Where(x => x.Id == EmployeeID)
+                .FirstOrDefault();
+        }
+
+        private IEnumerable<Employee> GetEmployeeList()
+        {
+            Client client = new Client(new Uri("http://masglobaltestapi.azurewebsites.net/api/Employees"));
+            Task<RestClient.Net.Abstractions.Response<List<Employee>>> response = client.GetAsync<List<Employee>>();
+            return response.Result.Body;
         }
     }
 }
