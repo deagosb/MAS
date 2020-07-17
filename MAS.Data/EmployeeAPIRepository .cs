c9f00b97dc8db73f493f49237ff94c5057686c63
Barrero Diego <deagosb@gmail.com>
2020-07-17 15:32:56 -0500

    Add EmployeeAPIRepository

---
+++ MAS.Data/EmployeeAPIRepository .cs
@@ -0,0 +1,18 @@
+﻿using MAS.Domain;
+using System.Collections.Generic;
+
+namespace MAS.Data
+{
+    public class EmployeeAPIRepository : IEmployeeRepository
+    {
+        public IEnumerable<Employee> GetAll()
+        {
+            return new List<Employee>();
+        }
+
+        public Employee GetById(int EmployeeID)
+        {
+            return new Employee();
+        }
+    }
+}
--- MAS.Data/IEmployeeRepository.cs
+++ MAS.Data/IEmployeeRepository.cs
@@ -7,7 +7,7 @@ namespace MAS.Data
     {
         IEnumerable<Employee> GetAll();
         Employee GetById(int EmployeeID);
-        void Insert(Employee employee);
+        //void Insert(Employee employee);
         //void Update(Employee employee);
         //void Delete(int EmployeeID);
         //void Save();
